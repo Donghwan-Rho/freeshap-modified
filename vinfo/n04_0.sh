ef545adc4c0fb60a39793f9eceeaac830c5a7284
Donghwan-Rho <donghwan_rho@snu.ac.kr>
2026-02-13 15:34:36 +0900

    260213

---
+++ vinfo/n04_0.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+python task_ntk.py --dataset_name qqp --num_train_dp 1000 --val_sample_num 1000
+python task_shapley_acc.py --dataset_name qqp --num_train_dp 1000 --val_sample_num 1000  --approximate inv --lambda_ 1e-6
+python task_shapley_acc.py --dataset_name qqp --num_train_dp 1000 --val_sample_num 1000 --approximate eigen --eigen_rank 30 --lambda_ 1e-6
+python task_shapley_acc.py --dataset_name qqp --num_train_dp 1000 --val_sample_num 1000 --approximate eigen --eigen_rank 30 --lambda_ 1e-5
+python task_shapley_acc.py --dataset_name qqp --num_train_dp 1000 --val_sample_num 1000 --approximate eigen --eigen_rank 30 --lambda_ 1e-4
+python task_shapley_acc.py --dataset_name qqp --num_train_dp 1000 --val_sample_num 1000 --approximate eigen --eigen_rank 30 --lambda_ 1e-3
+python task_shapley_acc.py --dataset_name qqp --num_train_dp 1000 --val_sample_num 1000 --approximate eigen --eigen_rank 30 --lambda_ 1e-2
+python task_shapley_acc.py --dataset_name qqp --num_train_dp 1000 --val_sample_num 1000 --approximate eigen --eigen_rank 30 --lambda_ 1e-1
+# CUDA_VIISBLE_DEVICES=0 python prompt_finetune_train.py --num_train_dp 1000 --tmc_iter 500 --approximate inv --lambda_ 1e-6  --sweep_percentages --sweep_start 1 --sweep_end 100 --sweep_step 1
+# CUDA_VIISBLE_DEVICES=0,1 python prompt_finetune_train.py --num_train_dp 10000 --tmc_iter 500 --approximate inv --lambda_ 1e-6  --sweep_percentages --sweep_start 1 --sweep_end 100 --sweep_step 1
